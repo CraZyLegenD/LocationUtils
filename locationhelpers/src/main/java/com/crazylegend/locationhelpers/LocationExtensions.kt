c0018ae1f9c55dca90af1163db8e130c24503252
CraZyLegenD <crazyxylene@gmail.com>
2020-06-14 13:11:03 +0200

    meters to km and otherwise

--- locationhelpers/src/main/java/com/crazylegend/locationhelpers/LocationExtensions.kt
+++ locationhelpers/src/main/java/com/crazylegend/locationhelpers/LocationExtensions.kt
@@ -4,6 +4,7 @@ import android.location.Location
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
 
 
 /**
@@ -32,4 +33,72 @@ fun LatLng.changeMarkerOffsetCenter(googleMap: GoogleMap, yOffset: Int = 120) {
     val mapPoint = googleMap.projection.toScreenLocation(this)
     mapPoint[mapPoint.x] = mapPoint.y - yOffset
     googleMap.animateCamera(CameraUpdateFactory.newLatLng(googleMap.projection.fromScreenLocation(mapPoint)))
+}
+
+
+fun metersToKMH(speed: Double) = speed * 3600 / 1000
+fun metersToKMH(speed: Float) = speed * 3600 / 1000
+
+
+fun metersToKM(meters: Double) = meters / 1000
+fun kilometersToMeters(km: Double) = km * 1000
+
+fun metersToKM(meters: Float) = meters / 1000
+fun kilometersToMeters(km: Float) = km * 1000
+
+
+/**
+ * Calculates the speed in m/s
+ * @param oldLocation Location
+ * @param newLocation Location
+ * @param tolerance tolerance in meters
+ * @return Double
+ */
+fun getSpeed(oldLocation: Location, newLocation: Location, tolerance: Int = 80): Double {
+    val realDistance = getRealDistance(oldLocation, newLocation, tolerance)
+
+    return if (oldLocation.hasSpeed() && newLocation.hasSpeed()) {
+        val locationSpeed = listOf(oldLocation.speed, newLocation.speed)
+        locationSpeed.max()?.toDouble() ?: 0.toDouble()
+    } else {
+        return if (realDistance != 0.toDouble()) {
+
+            val timeOld = oldLocation.time / 1000
+            val timeNew = newLocation.time / 1000
+
+            realDistance / (timeNew - timeOld).toDouble()
+
+        } else {
+            0.toDouble()
+        }
+    }
+
+}
+
+/**
+ * Calculates distance to another location in km
+ * @receiver Location
+ * @param anotherLocation Location
+ * @return Float
+ */
+fun Location.distanceToInKM(anotherLocation: Location) = metersToKM(this.distanceTo(anotherLocation))
+
+/**
+ * returns the distance in meters
+ * @param location1 Location
+ * @param location2 Location
+ * @param tolerance tolerance in meters
+ * @return Double
+ */
+fun getRealDistance(location1: Location, location2: Location, tolerance: Int = 80): Double {
+    val latLng1 = LatLng(location1.latitude, location1.longitude)
+    val latLng2 = LatLng(location2.latitude, location2.longitude)
+
+    return with(SphericalUtil.computeDistanceBetween(latLng1, latLng2)) {
+        if (this >= tolerance) {
+            0.toDouble()
+        } else {
+            this
+        }
+    }
 }
